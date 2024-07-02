9ae513ad10ab4cacabc7ae534f30c582c398a1a1
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-07-02 18:13:27 +0400

    feat: add abstract view class

---
+++ src/app.js
@@ -0,0 +1,14 @@
+export class App {
+  routes = [{ path: '', view: null }];
+
+  constructor() {
+    window.addEventListener('hashchange', this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    const view = this.routes.find(r => r.path == location.hash).view;
+  }
+}
+
+new App();
