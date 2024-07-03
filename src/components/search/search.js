898ee54c27500614cd3c762bace70950c0729671
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-07-03 15:21:33 +0400

    feat: add async books load by search query

---
+++ src/components/search/search.js
@@ -0,0 +1,47 @@
+import { DivComponent } from '../../common/div-component';
+import './search.css';
+
+export class Search extends DivComponent {
+  constructor(state) {
+    super();
+    this.state = state;
+  }
+
+  search() {
+    const value = this.el.querySelector('input').value;
+    this.state.searchQuery = value;
+  }
+
+  render() {
+    this.el.classList.add('search');
+    this.el.innerHTML = `
+      <div class='search__wrapper'>
+        <input
+          type='text'
+          placeholder='Найти книгу или автора...'
+          class='search__input'
+          value='${this.state.searchQuery || ''}'
+        />
+        <img src="/static/search.svg" alt="Поиск" />
+      </div>
+      <button>
+        <img
+          src="static/search-light.svg"
+          alt="Поиск книг"
+          aria-label="Искать"
+        />
+      </button>
+    `;
+
+    this.el
+      .querySelector('button')
+      .addEventListener('click', this.search.bind(this));
+    this.el.querySelector('input').addEventListener('keydown', e => {
+      if (e.code === 'Enter') {
+        this.search();
+      }
+    });
+
+    return this.el;
+  }
+}
